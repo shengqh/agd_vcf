f625b449ae7821462a314e8eeea27fa356719388
Quanhu Sheng <shengqh@gmail.com>
2025-01-06 17:34:17 -0600

    batch and result

--- batch.sh
+++ batch.sh
@@ -13,7 +13,7 @@ fi
 # 1. Test agd_vcf
 if [[ ! -s agd_vcf.vcf.gz ]]; then
   echo "Testing agd_vcf..."
-  echo "\nagd_vcf:" >> $TIME_COST_FILE
+  echo "agd_vcf:" >> $TIME_COST_FILE
   { time zcat $DRAGEN_VCF | ./agd_vcf --id_map_file=$ID_MAP_FILE | bgzip > agd_vcf.vcf.gz ; } 2>> $TIME_COST_FILE
   zcat agd_vcf.vcf.gz | grep -v "^#" | wc -l >> $TIME_COST_FILE
 fi
@@ -21,7 +21,7 @@ fi
 # 2. Test bcftools
 if [[ ! -s bcftools.vcf.gz ]]; then
   echo "Testing bcftools..."
-  echo "\nbcftools:" >> $TIME_COST_FILE
+  echo "bcftools:" >> $TIME_COST_FILE
   { time bcftools view -f PASS $DRAGEN_VCF -O v | bgzip > bcftools.vcf.gz ; } 2>> $TIME_COST_FILE
   zcat bcftools.vcf.gz | grep -v "^#" | wc -l >> $TIME_COST_FILE
 fi
@@ -29,9 +29,31 @@ fi
 # 3. Test awk
 if [[ ! -s awk.vcf.gz ]]; then
   echo "Testing awk..."
-  echo "\nawk:" >> $TIME_COST_FILE
+  echo "awk:" >> $TIME_COST_FILE
   { time zcat $DRAGEN_VCF | awk '$7 == "PASS" || $1 ~ /^#/' | bgzip > awk.vcf.gz ; } 2>> $TIME_COST_FILE
   zcat awk.vcf.gz | grep -v "^#" | wc -l >> $TIME_COST_FILE
+fi
+
+# test bgzip thread 4
+# 1. Test agd_vcf
+if [[ ! -s agd_vcf.thread4.vcf.gz ]]; then
+  echo "Testing agd_vcf.thread4..."
+  echo "agd_vcf.thread4:" >> $TIME_COST_FILE
+  { time zcat $DRAGEN_VCF | ./agd_vcf --id_map_file=$ID_MAP_FILE | bgzip -@ 4 -c > agd_vcf.thread4.vcf.gz ; } 2>> $TIME_COST_FILE
+fi
+
+# 2. Test bcftools
+if [[ ! -s bcftools.thread4.vcf.gz ]]; then
+  echo "Testing bcftools.thread4..."
+  echo "bcftools.thread4:" >> $TIME_COST_FILE
+  { time bcftools view -f PASS $DRAGEN_VCF -O v | bgzip -@ 4 -c > bcftools.vcf.gz ; } 2>> $TIME_COST_FILE
+fi
+
+# 3. Test awk
+if [[ ! -s awk.thread4.vcf.gz ]]; then
+  echo "Testing awk.thread4..."
+  echo "awk.thread4:" >> $TIME_COST_FILE
+  { time zcat $DRAGEN_VCF | awk '$7 == "PASS" || $1 ~ /^#/' | bgzip -@ 4 -c > awk.thread4.vcf.gz ; } 2>> $TIME_COST_FILE
 fi
 
 echo "Time cost saved to $TIME_COST_FILE"
