2a69bbf55e363410ad6731c58a8cd6314dcab3aa
Quanhu Sheng <shengqh@gmail.com>
2026-03-23 11:18:13 -0500

    add batch test

--- batch.sh
+++ batch.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # write batch test for agd_vcf, bcftools and awk
 
-DRAGEN_VCF="/nobackup/h_cqs/shengq2/biovu/agd163k/chrom-msvcf/chrom-25-M/postproc-vcf/dragen.vcf.gz"
-ID_MAP_FILE="/nobackup/h_cqs/shengq2/biovu/AGD/SAMPLE_ID_MAP_2024_Q2_163K_primaryGRID_v5_202410.txt"
+DRAGEN_VCF="/nobackup/h_cqs/shengq2/biovu/agd250k/chrom-msvcf/chrom-25-M/postproc-vcf/dragen.vcf.gz"
 
 # Output file for time costs
 TIME_COST_FILE="time_cost.txt"
@@ -14,7 +13,7 @@ fi
 if [[ ! -s agd_vcf.vcf.gz ]]; then
   echo "Testing agd_vcf..."
   echo "agd_vcf:" >> $TIME_COST_FILE
-  { time zcat $DRAGEN_VCF | ./agd_vcf --id_map_file=$ID_MAP_FILE --total_variants=52402 | bgzip > agd_vcf.vcf.gz ; } 2>> $TIME_COST_FILE
+  { time zcat $DRAGEN_VCF | ./agd_vcf --total_variants=60271 | bgzip > agd_vcf.vcf.gz ; } 2>> $TIME_COST_FILE
   zcat agd_vcf.vcf.gz | grep -v "^#" | wc -l >> $TIME_COST_FILE
 fi
 
@@ -24,6 +23,14 @@ if [[ ! -s bcftools.vcf.gz ]]; then
   echo "bcftools:" >> $TIME_COST_FILE
   { time bcftools view -f PASS $DRAGEN_VCF -O v | bgzip > bcftools.vcf.gz ; } 2>> $TIME_COST_FILE
   zcat bcftools.vcf.gz | grep -v "^#" | wc -l >> $TIME_COST_FILE
+fi
+
+# 2. Test bcftools 8 threads
+if [[ ! -s bcftools.threads8.vcf.gz ]]; then
+  echo "Testing bcftools with thread 8..."
+  echo "bcftools:" >> $TIME_COST_FILE
+  { time bcftools view --threads 8 -f PASS $DRAGEN_VCF -O v | bgzip > bcftools.threads8.vcf.gz ; } 2>> $TIME_COST_FILE
+  zcat bcftools.threads8.vcf.gz | grep -v "^#" | wc -l >> $TIME_COST_FILE
 fi
 
 # 3. Test awk
@@ -39,7 +46,7 @@ fi
 if [[ ! -s agd_vcf.thread4.vcf.gz ]]; then
   echo "Testing agd_vcf.thread4..."
   echo "agd_vcf.thread4:" >> $TIME_COST_FILE
-  { time zcat $DRAGEN_VCF | ./agd_vcf --id_map_file=$ID_MAP_FILE --total_variants=52402 | bgzip -@ 4 -c > agd_vcf.thread4.vcf.gz ; } 2>> $TIME_COST_FILE
+  { time zcat $DRAGEN_VCF | ./agd_vcf --total_variants=60271 | bgzip -@ 4 -c > agd_vcf.thread4.vcf.gz ; } 2>> $TIME_COST_FILE
 fi
 # 2. Test awk
 if [[ ! -s awk.thread4.vcf.gz ]]; then
